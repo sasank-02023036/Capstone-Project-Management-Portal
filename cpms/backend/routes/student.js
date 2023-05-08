df2b6ee164ef4092397a486e092ba88d7a26cc03
Rakshith Reddy Madavapeddi <113482488+RakshithMadavapeddi@users.noreply.github.com>
2023-05-07 21:43:45 -0400

    Delete cpms directory

--- cpms/backend/routes/student.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getStudentDashboard } = require('../controllers/student');
-
-// Route to get the student dashboard
-router.get('/dashboard', getStudentDashboard);
-
-module.exports = router;
