cbd6d2dbff525f66361d857ce07a33078fb9ec9f
bapiraju001 <114434448+bapiraju001@users.noreply.github.com>
2023-05-22 12:58:57 -0400

    Delete server_side/routes directory

--- server_side/routes/preferenceRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const preferenceController = require("../controllers/preferrenceController");
-
-router.get('/preference/:name', preferenceController.getPreferencesByCourse);
-
-router.put('/preference' , preferenceController.updatePreference);
-
-module.exports = router;
