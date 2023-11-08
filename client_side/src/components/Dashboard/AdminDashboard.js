1d772e03bb1ff09cd572d145516a53f66eb3d98c
sasank-02023036 <sasank.varanasi001@umb.edu>
2023-11-08 01:41:27 -0500

    my changes

--- client_side/src/components/Dashboard/AdminDashboard.js
+++ client_side/src/components/Dashboard/AdminDashboard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "components/Header/navbar";
-import { Header, Card } from 'semantic-ui-react'
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
