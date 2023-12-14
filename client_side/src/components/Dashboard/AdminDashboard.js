94cf80021213bf008f1cd94294a9014faf5faaa0
sasank-02023036 <sasank.varanasi001@umb.edu>
2023-12-14 12:18:17 -0500

    trying to pull changes

--- client_side/src/components/Dashboard/AdminDashboard.js
+++ client_side/src/components/Dashboard/AdminDashboard.js
@@ -143,9 +143,9 @@ export default function AdminDashboard() {
                 
                 
             </Grid>
+            
 
-            <Footer />
-            <FooterPlain />
+            
             
         </>
     )
